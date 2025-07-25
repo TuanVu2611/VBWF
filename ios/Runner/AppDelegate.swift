99f2f960eb1abad46187a460a1aadd05892f4178
linda <vudv_b01537@fpt.aptech.ac.vn>
2025-07-25 16:41:11 +0700

    ok

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,10 +11,13 @@ import flutter_local_notifications
     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
       GeneratedPluginRegistrant.register(with: registry)
     }
+
     GeneratedPluginRegistrant.register(with: self)
-    if(#available(iOS 10.0, *)) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
     }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
